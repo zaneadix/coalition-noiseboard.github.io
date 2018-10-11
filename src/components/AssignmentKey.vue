ae34cdd7e3fc400f514c7f83411b0ce646d7a1f2
Zane Adickes <zaneadix@gmail.com>
2018-10-11 18:37:07 -0400

    ayaotd music

--- src/components/AssignmentKey.vue
+++ src/components/AssignmentKey.vue
@@ -1,18 +1,10 @@
 
 <template>
 
-    <div class="key assignment-key"
-         v-on:click="checkClick($event, 'assign')">
-        <div class="key-body"
-             ref="keyBody"
-             @mouseenter="hover = true"
-             @mouseleave="hover = false">
+    <div class="key assignment-key" v-on:click="checkClick($event, 'assign')">
+        <div class="key-body" ref="keyBody" @mouseenter="hover = true" @mouseleave="hover = false">
 
-            <button
-                class="unassigner icon-button"
-                ref="unassignerBtn"
-                v-if="hover && noise"
-                v-on:click="checkClick($event, 'unassign')">
+            <button class="unassigner icon-button" ref="unassignerBtn" v-if="hover && noise" v-on:click="checkClick($event, 'unassign')">
                 <svg class="icon" ref="unassignerSvg">
                     <use xlink:href="#icon-x-square"></use>
                 </svg>
@@ -23,59 +15,51 @@
             <small v-else class="soft-text">empty</small>
         </div>
     </div>
-    
+
 </template>
 
 <script>
+import { mapState, mapActions } from "vuex";
 
-    import { mapState, mapActions } from 'vuex';
-    
-    export default {
-        name: 'assignment-key',
+export default {
+  name: "assignment-key",
 
-        props: [
-            'character',
-            'noise'
-        ],
+  props: ["character", "noise"],
 
-        data: function () {
-            return {
-                hover: false
-            }
-        },
+  data: function() {
+    return {
+      hover: false
+    };
+  },
 
-        methods: {
-
-            checkClick: function (event) {
-                switch (event.target) {
-                    case this.$refs.unassignerSvg:
-                    case this.$refs.unassignerBtn:
-                        this.$emit('unassign', this.character);
-                        break;
-                    default:
-                        this.$emit('assign', this.character);
-                }
-            }
-        }
+  methods: {
+    checkClick: function(event) {
+      switch (event.target) {
+        case this.$refs.unassignerSvg:
+        case this.$refs.unassignerBtn:
+          this.$emit("unassign", this.character);
+          break;
+        default:
+          this.$emit("assign", this.character);
+      }
     }
-
+  }
+};
 </script>
 
 
 <style lang="scss" scoped="true">
+@import "../styles/variables";
 
-    @import '../styles/variables';
-
-    .key-body {
-        .unassigner {
-            position: absolute;
-            top: 3px;
-            right: 3px;
-            svg {
-                height: 1rem;
-                width: 1rem;
-            }
-        }
+.key-body {
+  .unassigner {
+    position: absolute;
+    top: 3px;
+    right: 3px;
+    svg {
+      height: 1rem;
+      width: 1rem;
     }
-
+  }
+}
 </style>
